d2de9e2abd6bc584fbc0df5edd59c5044f1f8ea6
Robert-Collins-CS <collinro@oregonstate.edu>
2021-04-16 18:24:31 -0700

    Finished password generator, includes function call

--- passGen.py
+++ passGen.py
@@ -5,10 +5,15 @@ LETTERS = string.ascii_letters
 NUMBERS = string.digits
 
 def password_generator(length=8): 
-   printable = f'{LETTERS}{NUMBERS}'
+   alphaNum = f'{LETTERS}{NUMBERS}'
 
-   printable = list(printable)
-   random.shuffle(printable)
+   alphaNum = list(alphaNum)
+   random.shuffle(alphaNum)
  
+   gen_password = random.choices(alphaNum, k=length)
+   gen_password = ''.join(gen_password)
+   print(gen_password)
 
-   return random_password
+   return gen_password
+
+password_generator(int(input("Enter a Number: ")))   
